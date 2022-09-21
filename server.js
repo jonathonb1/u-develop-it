c1fb39151c96379ba15556a86aa54e06b06b5162
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-09-20 18:48:44 -0700

    added delete statement

--- server.js
+++ server.js
@@ -20,12 +20,26 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
 });
 
-
-
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 
 
@@ -34,6 +48,8 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
 app.use((req, res) => {
     res.status(404).end();
 });
+
+
 
 
 app.listen(PORT, () => {
